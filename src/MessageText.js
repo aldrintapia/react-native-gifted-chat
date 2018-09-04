fb622b231e418340346e1d97b774e47cbf9a0287
aldrintapia <aldrin_tapia@yahoo.com>
2018-09-04 18:30:54 +0800

    Update MessageText.js

--- src/MessageText.js
+++ src/MessageText.js
@@ -78,7 +78,7 @@ export default class MessageText extends React.Component {
             styles[this.props.position].text,
             this.props.textStyle[this.props.position],
             this.props.customTextStyle,
-            {marginTop:{(this.props.currentMessage.isOffline)?10:0}
+            {marginTop:(this.props.currentMessage.isOffline)?15:0}
           ]}
           parse={[
             ...this.props.parsePatterns(linkStyle),
